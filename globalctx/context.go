e1aaddb43a140071622ccfb66b266f8f0670af5d
Baptiste Canton <bat@sbz.fr>
2022-06-11 01:54:46 +0200

    protect globalCtx w/ a mutex

--- globalctx/context.go
+++ globalctx/context.go
@@ -3,19 +3,29 @@ package globalctx
 import (
 	"ccat/log"
 	"context"
+	"sync"
 )
 
-var globalCtx context.Context
+var globalCtx ctx
+
+type ctx struct {
+	ctx context.Context
+	mu  sync.Mutex
+}
 
 func init() {
-	globalCtx = context.Background()
+	globalCtx = ctx{ctx: context.Background()}
 }
 
 func Set(k string, v interface{}) {
+	globalCtx.mu.Lock()
+	defer globalCtx.mu.Unlock()
 	log.Debugf("globalctx: setting %v=%v\n", k, v)
-	globalCtx = context.WithValue(globalCtx, k, v)
+	globalCtx.ctx = context.WithValue(globalCtx.ctx, k, v)
 }
 
 func Get(k string) interface{} {
-	return globalCtx.Value(k)
+	globalCtx.mu.Lock()
+	defer globalCtx.mu.Unlock()
+	return globalCtx.ctx.Value(k)
 }
