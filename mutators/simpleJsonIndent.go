a0234074862e6709e586502da61b70911e41eb12
Baptiste Canton <bat@sbz.fr>
2022-06-15 17:35:41 +0200

    fix empty string in simpleJsonIndent

--- mutators/simpleJsonIndent.go
+++ mutators/simpleJsonIndent.go
@@ -17,12 +17,17 @@ func jsonIndent(w io.WriteCloser, r io.ReadCloser) (int64, error) {
 		return 0, err
 	}
 	var b bytes.Buffer
-	err = json.Indent(&b, j, "", "  ")
-	if err != nil {
-		return 0, err
+	if len(j) != 0 {
+		err = json.Indent(&b, j, "", "  ")
+		if err != nil {
+			return 0, err
+		}
+		b.WriteString("\n")
+		_, err = io.Copy(w, bytes.NewReader(b.Bytes()))
+		if err != nil {
+			return 0, err
+		}
 	}
-	b.WriteString("\n")
-	io.Copy(w, bytes.NewReader(b.Bytes()))
 
 	return int64(len(j)), nil
 }
