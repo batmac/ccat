7264d5bd37be08d71cbe79618fcb95b94982eb41
Baptiste Canton <bat@sbz.fr>
2022-06-17 22:32:51 +0200

    add go generate

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -x
 
 GIT=$(git tag|tail -n1)
 VERSION=">$GIT+dev"
-COMMIT="none"
+COMMIT=$(git rev-parse HEAD)
 DATE=$(date +%Y-%m-%d@%H:%M:%S)
 BUILTBY="build.sh"
 TAGS=libcurl,crappy
@@ -13,6 +13,7 @@ VARS="-X main.version=$VERSION -X main.commit=$COMMIT -X main.date=$DATE -X main
 CURLDIR="/opt/homebrew/opt/curl"
 export CGO_LDFLAGS="-L $CURLDIR/lib/"
 export CGO_CPPFLAGS="-I $CURLDIR/include/curl/"
+go generate
 go test -tags $TAGS ./...
 go install -v  -ldflags "-s -w $VARS" -tags $TAGS
 go build -v  -ldflags "-s -w $VARS" -tags $TAGS .
