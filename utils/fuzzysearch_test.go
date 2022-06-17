9928f70758908dead9f1b0e82ed4219b924d422b
Baptiste Canton <bat@sbz.fr>
2022-06-17 14:06:19 +0200

    oups

---
+++ utils/fuzzysearch_test.go
@@ -0,0 +1,34 @@
+package utils
+
+import "testing"
+
+func TestFuzzySearch(t *testing.T) {
+	type args struct {
+		str       string
+		strList   []string
+		threshold float32
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{"test", args{"tsest", []string{"first", "test", "notest", "cat", ""}, 0.5}, "test", false},
+		{"tet", args{"tet", []string{"first", "test", "notest", "cat", ""}, 0.5}, "test", false},
+		{"estt", args{"estt", []string{"first", "test", "notest", "cat", ""}, 0.5}, "test", false},
+		{"markdown", args{"markdown", []string{"test", "md", "notest", "cat", ""}, 0.5}, "md", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FuzzySearch(tt.args.str, tt.args.strList, tt.args.threshold)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FuzzySearch() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("FuzzySearch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
