a5a87f3ffc4a0e324a857c2684ba7fe9b63601cc
Baptiste Canton <bat@sbz.fr>
2022-06-21 22:05:59 +0200

    register zstd as zip decompressor

--- mutators/simpleZstd.go
+++ mutators/simpleZstd.go
@@ -1,18 +1,27 @@
 package mutators
 
 import (
+	"archive/zip"
 	"io"
 
 	"github.com/klauspost/compress/zstd"
 )
 
 func init() {
+	registerAsZipDecompressor()
+
 	simpleRegister("unzstd", unzstd, withDescription("decompress zstd data"),
 		withCategory("decompress"),
 	)
 	simpleRegister("zstd", czstd, withDescription("compress to zstd data"),
 		withCategory("compress"),
 	)
+}
+
+func registerAsZipDecompressor() {
+	decomp := zstd.ZipDecompressor()
+	zip.RegisterDecompressor(zstd.ZipMethodWinZip, decomp)
+	zip.RegisterDecompressor(zstd.ZipMethodPKWare, decomp)
 }
 
 func unzstd(out io.WriteCloser, in io.ReadCloser) (int64, error) {
