52f9102861bb0fb58f0910d02c7e2a1ab7f39bf1
Baptiste Canton <bat@sbz.fr>
2022-06-27 23:08:49 +0200

    remove a test for go1.15

--- log/log_test.go
+++ log/log_test.go
@@ -1,39 +1,38 @@
-//go:build !go1.15
-// +build !go1.15
-
-package log
+package log_test
 
 import (
 	"bytes"
-	"log"
-	"reflect"
 	"strings"
 	"testing"
+
+	// stdlog "log"
+
+	"github.com/batmac/ccat/log"
 )
 
-func TestDefault(t *testing.T) {
+/* func TestDefault(t *testing.T) {
 	tests := []struct {
 		name string
-		want *Logger
+		want *log.Logger
 	}{
-		{"empty", &Logger{}},
-		{"default", &Logger{log.Default()}},
+		{"empty", &log.Logger{}},
+		{"default", &log.Logger{stdlog.Default()}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Default(); reflect.TypeOf(got) != reflect.TypeOf(tt.want) {
+			if got := log.Default(); reflect.TypeOf(got) != reflect.TypeOf(tt.want) {
 				t.Errorf("Default() = %v, want %v", got, tt.want)
 			}
 		})
 	}
-}
+} */
 
 func TestSetDebug(t *testing.T) {
 	t.Run("bytes.Buffer", func(t *testing.T) {
 		w := &bytes.Buffer{}
-		SetDebug(w)
-		if w != (Debug.Logger.Writer()) {
-			t.Errorf("SetDebug() = %v, want %v", Debug.Logger, w)
+		log.SetDebug(w)
+		if w != (log.Debug.Logger.Writer()) {
+			t.Errorf("SetDebug() = %v, want %v", log.Debug.Logger, w)
 		}
 	})
 }
@@ -47,7 +46,6 @@ func TestPp(t *testing.T) {
 		args args
 		want string
 	}{
-		// TODO: Add test cases.
 		{"null", args{nil}, "null"},
 		{"zero", args{0}, "0"},
 		{"empty", args{struct{}{}}, "{}"},
@@ -56,7 +54,7 @@ func TestPp(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(Pp(tt.args.data), "\n", ""), "\t", ""), " ", ""); got != tt.want {
+			if got := strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(log.Pp(tt.args.data), "\n", ""), "\t", ""), " ", ""); got != tt.want {
 				t.Errorf("Pp() = %+v, want %+v", got, tt.want)
 			}
 		})
