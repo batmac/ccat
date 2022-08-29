4d69d0f953fe77b528420adf2dec7a18869c549a
Baptiste Canton <bat@sbz.fr>
2022-08-29 12:07:51 +0200

    rename utils/ stuff

---
+++ pkg/utils/deleteSpaces.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"strings"
+	"unicode"
+)
+
+// https://stackoverflow.com/questions/32081808/strip-all-whitespace-from-a-string
+func DeleteSpaces(str string) string {
+	var b strings.Builder
+	b.Grow(len(str))
+	for _, ch := range str {
+		if !unicode.IsSpace(ch) {
+			b.WriteRune(ch)
+		}
+	}
+	return b.String()
+}
---
+++ pkg/utils/deleteSpaces_test.go
@@ -0,0 +1,36 @@
+package utils_test
+
+import (
+	"testing"
+
+	"github.com/batmac/ccat/pkg/utils"
+)
+
+func TestDeleteSpaces(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"test", args{"test"}, "test"},
+		{"test ", args{"test "}, "test"},
+		{" test ", args{"test "}, "test"},
+		{"  test  ", args{"  test  "}, "test"},
+		{" te st ", args{"test "}, "test"},
+		{"test\n", args{"test\n"}, "test"},
+		{"\nte\nst\n", args{"\nte\nst\n"}, "test"},
+		{"\ntest\n", args{"\ntest\n"}, "test"},
+		{"te\t\n st", args{"te\t\n st"}, "test"},
+		{"te  \u00a0st", args{"te  \u00a0st"}, "test"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := utils.DeleteSpaces(tt.args.str); got != tt.want {
+				t.Errorf("DeleteSpaces() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
