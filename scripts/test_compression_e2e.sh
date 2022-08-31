d24db6c7cf7b086627b520e040ab572c83ed3994
Baptiste Canton <bat@sbz.fr>
2022-08-31 23:17:36 +0200

    add missing bzip2 e2e test

--- scripts/test_compression_e2e.sh
+++ scripts/test_compression_e2e.sh
@@ -6,13 +6,13 @@ CKSUM=md5sum
 DIR=$1
 
 for FILE in $DIR/* ; do
-	echo testing $FILE
-	EXPECTED=$($CKSUM $FILE| cut -d" " -f1)
-	echo expected checksum is $EXPECTED
-	for a in gzip lz4 lzma2 lzma s2 snap xz zlib zip zstd ; do
+	echo testing "$FILE"
+	EXPECTED=$($CKSUM "$FILE"| cut -d" " -f1)
+	echo expected checksum is "$EXPECTED"
+	for a in bzip2 gzip lz4 lzma2 lzma s2 snap xz zip zlib zstd ; do
 		echo testing $a
-		SUM=$($CAT $FILE| $CCAT -m $a | $CCAT -m un$a | $CKSUM | cut -d" " -f1)
-		echo $SUM
+		SUM=$($CAT "$FILE"| $CCAT -m $a | $CCAT -m un$a | $CKSUM | cut -d" " -f1)
+		echo "$SUM"
 		[ "$SUM" = "$EXPECTED" ] || (echo FAILED && exit 1)
 	done
 done
