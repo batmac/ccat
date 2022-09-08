b9bd57dd693d712711bb056f73beb3d437ef3d46
Baptiste Canton <bat@sbz.fr>
2022-09-08 17:42:27 +0200

    improve mage build (#132)
    * convert compression test to mage
    * moar stuff
    * skip this test on windows

--- cmd/ccat/generated_test.go
+++ cmd/ccat/generated_test.go
@@ -4,10 +4,14 @@ import (
 	"bytes"
 	"os"
 	"reflect"
+	"runtime"
 	"testing"
 )
 
 func Test_printLicense(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("skipped on windows because crlf stuff.")
+	}
 	l, err := os.ReadFile("../../LICENSE")
 	if err != nil {
 		t.Fatal(err)
--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -4,7 +4,7 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
+	"path/filepath"
 	"runtime"
 	"time"
 
@@ -29,18 +29,18 @@ func init() {
 }
 
 func ldFlags(goTags string) string {
-	version, err := exec.Command("git", "describe", "--tags").Output()
+	version, err := sh.Output("git", "describe", "--tags")
 	if err != nil {
 		_ = mg.Fatal(1, err)
 	}
-	commit, err := exec.Command("git", "rev-parse", "HEAD").Output()
+	commit, err := sh.Output("git", "rev-parse", "HEAD")
 	if err != nil {
 		_ = mg.Fatal(1, err)
 	}
 
 	return fmt.Sprintf("-s -w -X main.version=%s -X main.commit=%s -X main.date=%s -X main.builtBy=%s -X main.tags=%s",
-		string(version),
-		string(commit),
+		version,
+		commit,
 		time.Now().Format("2006-01-02@15:04:05-0700"),
 		"Mage",
 		goTags,
@@ -54,7 +54,7 @@ func build(tags string) error {
 	if err != nil {
 		return err
 	}
-	if err := os.Chdir("cmd/ccat"); err != nil {
+	if err := os.Chdir(filepath.FromSlash("cmd/ccat")); err != nil {
 		return err
 	}
 	buildArgs := defaultBuildArgs
@@ -64,13 +64,14 @@ func build(tags string) error {
 		return err
 	}
 
-	if err := os.Rename(binaryName, "../../"+binaryName); err != nil {
+	if err := os.Rename(binaryName, filepath.Join(cwd, binaryName)); err != nil {
 		return err
 	}
 	if err := os.Chdir(cwd); err != nil {
 		return err
 	}
 
+	stepOKPrintln("Building OK")
 	return nil
 }
 
@@ -90,52 +91,77 @@ func BuildFull() error {
 
 // put ccat to $GOPATH/bin/ccat
 func Install() error {
-	path := os.ExpandEnv("$GOPATH/bin/" + binaryName)
-	stepPrintf("Installing to '%s'...\n", path)
+	path := os.ExpandEnv(filepath.Join("$GOPATH", "bin", binaryName))
+	stepPrintf("Installing... (%s)\n", path)
 
 	data, err := os.ReadFile(binaryName)
 	if err != nil {
+		// detect this
 		fmt.Println("Have you build first?")
 		return err
 	}
-	return maybe.WriteFile(path, data, 0o750)
+	if err := maybe.WriteFile(path, data, 0o750); err != nil {
+		return err
+	}
+	stepOKPrintln("Installing OK")
+	return nil
 }
 
 // go mod download
 func InstallDeps() error {
 	stepPrintln("Installing Deps...")
-	return sh.RunV("go", "mod", "download")
+	if err := sh.RunV("go", "mod", "download"); err != nil {
+		return err
+	}
+	stepOKPrintln("Installing Deps OK")
+	return nil
 }
 
 // go mod verify
 func VerifyDeps() error {
 	mg.Deps(InstallDeps)
 	stepPrintln("Verifying Deps...")
-	return sh.Run("go", "mod", "verify")
+	if err := sh.Run("go", "mod", "verify"); err != nil {
+		return err
+	}
+	stepOKPrintln("Verifying Deps OK")
+	return nil
 }
 
-func Clean() {
+func Clean() error {
 	stepPrintln("Cleaning...")
-	_ = os.RemoveAll(binaryName)
+	if err := sh.Rm(binaryName); err != nil {
+		return err
+	}
+	stepOKPrintln("Cleaning OK")
+	return nil
 }
 
 // go test ./...
 func TestGo() error {
 	mg.Deps(InstallDeps)
 	stepPrintln("Testing Go...")
-	return sh.RunV("go", "test", "./...")
+	r, err := sh.Output("go", "test", "./...")
+	if mg.Verbose() {
+		fmt.Println(r, "\n ")
+	}
+	if err != nil {
+		return err
+	}
+	stepOKPrintln("Testing Go OK")
+	return nil
 }
 
 // test_compression_e2e
-func TestCompression() error {
+/* func TestCompression() error {
 	stepPrintln("Testing compression...")
 	return sh.RunV("scripts/test_compression_e2e.sh", "testdata/compression/")
-}
+} */
 
 // test all
 func Test() error {
 	mg.SerialDeps(TestGo)
-	mg.SerialDeps(TestCompression)
+	mg.SerialDeps(TestCompressionGo)
 	return nil
 }
 
@@ -143,7 +169,6 @@ func Test() error {
 func BuildDefaultAndTest() error {
 	mg.SerialDeps(BuildDefault)
 	mg.SerialDeps(Test)
-	stepPrintln("Done.")
 	return nil
 }
 
@@ -155,15 +180,19 @@ func UpdateREADME() error {
 		return err
 	}
 	data = append(data, "\n```\n"...)
-	cmd := exec.Command("./"+binaryName, "--fullhelp")
-	out, err := cmd.CombinedOutput()
+	out, err := sh.Output("./"+binaryName, "--fullhelp")
+
 	data = append(data, out...)
 	if err != nil {
 		return err
 	}
 	data = append(data, "```\n"...)
 
-	return os.WriteFile("README.md", data, 0o600)
+	if err := os.WriteFile("README.md", data, 0o600); err != nil {
+		return err
+	}
+	stepOKPrintln("Updating README.md OK")
+	return nil
 }
 
 func stepPrintln(a ...any) {
@@ -172,4 +201,12 @@ func stepPrintln(a ...any) {
 
 func stepPrintf(format string, a ...any) {
 	fmt.Printf("🚧 "+format, a...)
+}
+
+func stepOKPrintln(a ...any) {
+	prefix := ""
+	if !mg.Verbose() {
+		prefix = "\x1bM" // go on the previous line
+	}
+	fmt.Println(append([]any{prefix + "✅"}, a...)...)
 }
