fbfdca8c865ab4623db88ccb2291b27ec9b3d057
Baptiste Canton <bat@sbz.fr>
2022-10-01 14:38:05 +0200

    add a nacl e2e test

--- pkg/mutators/simple/nacl_test.go
+++ pkg/mutators/simple/nacl_test.go
@@ -28,3 +28,22 @@ func Test_simpleEasyOpen(t *testing.T) {
 		}
 	})
 }
+
+func Test_simpleNaclE2E(t *testing.T) {
+	t.Run("e2e", func(t *testing.T) {
+		// len(32) key (64 chars hex)
+		hexKey := "1234567890123456789012345678901234567890123456789012345678901234"
+		t.Setenv("KEY", hexKey)
+
+		seal := mutators.Run("easyseal", "hello")
+		if seal == "" {
+			t.Fatalf("mutators.Run() error : seal is empty")
+		}
+
+		opened := mutators.Run("easyopen", seal)
+
+		if opened != "hello" {
+			t.Errorf("mutators.Run() = '%v', want '%v'", opened, "hello")
+		}
+	})
+}
