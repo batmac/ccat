d1990d1130f616fa6bc049fc46ceb3ddc61f87e2
Baptiste Canton <bat@sbz.fr>
2023-01-12 00:19:58 +0100

    add ArgValidator stuff

--- pkg/mutators/simple/limit.go
+++ pkg/mutators/simple/limit.go
@@ -2,20 +2,40 @@ package mutators
 
 import (
 	"io"
-	"strconv"
+
+	"github.com/batmac/ccat/pkg/log"
+
+	"github.com/docker/go-units"
 )
 
 func init() {
-	simpleRegister("limit", limit, withDescription("a simple limiting fifo"))
+	simpleRegister("limit", limit, withDescription("a simple limiting fifo"),
+		withArgsValidator(limitValidator))
 }
 
 func limit(w io.WriteCloser, r io.ReadCloser, args ...string) (int64, error) {
-	bytes, err := strconv.ParseInt(args[0], 10, 64)
+	bytes, err := units.FromHumanSize(args[0])
 	if err != nil {
 		return 0, err
 	}
 
+	log.Debugf("limiting to %d bytes\n", bytes)
+
 	lr := io.LimitReader(r, bytes)
 
 	return io.Copy(w, lr) // streamable
+}
+
+func limitValidator(args []string) error {
+	if len(args) != 1 {
+		return ErrWrongNumberOfArgs(1, len(args))
+	}
+
+	if _, err := units.FromHumanSize(args[0]); err != nil {
+		return err
+	}
+	/* 	if _, err := strconv.ParseInt(args[0], 10, 64); err != nil {
+		return err
+	} */
+	return nil
 }
