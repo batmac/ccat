f90fff294b75d1d2310f7486d6e0f1fa19891d42
Baptiste Canton <bat@sbz.fr>
2023-01-12 01:10:27 +0100

    add limit_test.go

---
+++ pkg/mutators/simple/limit_test.go
@@ -0,0 +1,60 @@
+package mutators_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/batmac/ccat/pkg/mutators"
+)
+
+func Test_simpleLimit(t *testing.T) {
+	limitedSize := 10
+	tests := []struct {
+		name, decoded, encoded string
+	}{
+		{"hello", "hello", "hello"},
+		{"empty", "", ""},
+		{"zero", "\x00", "\x00"},
+		{
+			"long",
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			"Lorem ipsu",
+		},
+	}
+
+	f := "limit:" + fmt.Sprint(limitedSize)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mutators.Run(f, tt.decoded); got != tt.encoded {
+				t.Errorf("%s = %v, want %v", f, got, tt.encoded)
+			}
+		})
+	}
+}
+
+func Test_simpleLimitWithSISize(t *testing.T) {
+	limitedSizes := []string{
+		"10k", "10m", "10g", "10t", "10p", "10b",
+		"10kb", "10mb", "10gb", "10tb", "10pb",
+		"10K", "10M", "10G", "10T", "10P", "10B",
+		"10KB", "10MB", "10GB", "10TB", "10PB",
+	}
+	tests := []struct {
+		name, decoded, encoded string
+	}{
+		{"hello", "hello", "hello"},
+		{"empty", "", ""},
+		{"zero", "\x00", "\x00"},
+	}
+
+	for _, limitedSize := range limitedSizes {
+		f := "limit:" + limitedSize
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				if got := mutators.Run(f, tt.decoded); got != tt.encoded {
+					t.Errorf("%s = %v, want %v", f, got, tt.encoded)
+				}
+			})
+		}
+	}
+}
