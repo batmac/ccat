ad555ab7f300b42578d78c4f26b66e9bf673b318
Baptiste Canton <bat@sbz.fr>
2023-02-02 04:55:15 +0100

    convert -X to 'x' mutator (#238)
    * convert -X to 'x' mutator
    
    * bloop

---
+++ pkg/mutators/single/exec.go
@@ -0,0 +1,39 @@
+package mutators
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/batmac/ccat/pkg/log"
+)
+
+func init() {
+	singleRegister("x", pipedcmd, withDescription("execute command (e.g. 'x:head -n 10')"),
+		withConfigBuilder(stdConfigStringWithDefault("")))
+}
+
+func pipedcmd(w io.WriteCloser, r io.ReadCloser, config any) (int64, error) {
+	command := config.(string)
+
+	if command == "" {
+		return 0, fmt.Errorf("no command specified")
+	}
+
+	split := strings.Split(command, " ")
+
+	log.Debugf("executing command '%v'\n", split)
+	cmd := exec.Command(split[0], split[1:]...) //nolint:gosec // that's the point
+	cmd.Stdin = r
+	cmd.Stdout = w
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		return 0, err
+	}
+
+	return 0, nil
+}
