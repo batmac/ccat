688b131f81b1225d671f8fa3ca37cc1a7d2c2193
Baptiste Canton <bat@sbz.fr>
2023-02-03 13:55:54 +0100

    remove bytesForHumanString (useless code)

--- cmd/ccat/memoryUsage.go
+++ cmd/ccat/memoryUsage.go
@@ -5,15 +5,15 @@ import (
 	"os"
 	"runtime"
 
-	"github.com/batmac/ccat/pkg/stringutils"
+	"github.com/docker/go-units"
 )
 
 func PrintMemUsage() {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
 	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
-	fmt.Fprintf(os.Stderr, "Alloc = %v ", stringutils.BytesForHumanString(m.Alloc))
-	fmt.Fprintf(os.Stderr, "\tTotalAlloc = %v ", stringutils.BytesForHumanString(m.TotalAlloc))
-	fmt.Fprintf(os.Stderr, "\tSys = %v ", stringutils.BytesForHumanString(m.Sys))
+	fmt.Fprintf(os.Stderr, "Alloc = %v ", units.HumanSize(float64(m.Alloc)))
+	fmt.Fprintf(os.Stderr, "\tTotalAlloc = %v ", units.HumanSize(float64(m.TotalAlloc)))
+	fmt.Fprintf(os.Stderr, "\tSys = %v ", units.HumanSize(float64(m.Sys)))
 	fmt.Fprintf(os.Stderr, "\tNumGC = %v\n", m.NumGC)
 }
--- pkg/stringutils/bytesForHumanString.go
+++
@@ -1,9 +0,0 @@
-package stringutils
-
-import (
-	"github.com/docker/go-units"
-)
-
-func BytesForHumanString(b uint64) string {
-	return units.HumanSize(float64(b))
-}
