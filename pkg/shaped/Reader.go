c0ff458347e241241b91410610123737c3658d2c
Baptiste Canton <bat@sbz.fr>
2023-03-14 02:34:53 +0100

    add "Maxbw": a bandwidth limiter mutator (#273)
    * add maxbw mutator
    
    * renamed a few files
    
    * add maxbw_test.go

---
+++ pkg/shaped/Reader.go
@@ -0,0 +1,52 @@
+package shaped
+
+import (
+	"io"
+	"time"
+)
+
+type Reader struct {
+	r           io.Reader
+	bandwidth   int // Bytes per second
+	freeVoucher int
+	lastUpdate  time.Time
+	totalRead   int
+}
+
+// NewReader creates a new Reader.
+// bandwidth is the maximum of bytes to read per second.
+func NewReader(r io.Reader, bandwidth int) *Reader {
+	return &Reader{
+		r:          r,
+		bandwidth:  bandwidth,
+		lastUpdate: time.Now(),
+	}
+}
+
+func (lr *Reader) Read(p []byte) (int, error) {
+	now := time.Now()
+	elapsed := now.Sub(lr.lastUpdate).Seconds()
+	lr.lastUpdate = now
+
+	// Calculate the maximum number of bytes that can be read in the elapsed time
+	maxBytes := int(elapsed*float64(lr.bandwidth)+0.5) + lr.freeVoucher
+	lr.freeVoucher = 0
+
+	if maxBytes == 0 {
+		// for safety, should not occur frequently
+		maxBytes = 1
+		time.Sleep(time.Second)
+	}
+
+	// Limit the number of bytes read if the buffer is smaller than the computed maximum
+	if maxBytes > len(p) {
+		lr.freeVoucher = maxBytes - len(p)
+		maxBytes = len(p)
+	}
+
+	// Read from the underlying reader
+	n, err := lr.r.Read(p[:maxBytes])
+	lr.totalRead += n
+
+	return n, err
+}
