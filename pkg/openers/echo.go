9a9aa20a6eff6fa002dbda4e8954f0327474afc3
Baptiste Canton <bat@sbz.fr>
2023-04-03 12:49:06 +0200

    add echo:// opener

---
+++ pkg/openers/echo.go
@@ -0,0 +1,59 @@
+//go:build !fileonly
+// +build !fileonly
+
+package openers
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+var (
+	echoOpenerName        = "echo"
+	echoOpenerDescription = "echo the data given"
+)
+
+type echoOpener struct {
+	name, description string
+}
+
+func init() {
+	register(&echoOpener{
+		name:        echoOpenerName,
+		description: echoOpenerDescription,
+	})
+}
+
+func (f echoOpener) Name() string {
+	return f.name
+}
+
+func (f echoOpener) Description() string {
+	return f.description
+}
+
+func (f echoOpener) Open(s string, _ bool) (io.ReadCloser, error) {
+	var found bool
+	_, s, found = strings.Cut(s, "://")
+	if !found {
+		return nil, fmt.Errorf("no protocol given")
+	}
+	// remove quotes from start and end
+	s = strings.Trim(s, "\"'")
+
+	if len(s) == 0 {
+		return nil, fmt.Errorf("no data given")
+	}
+
+	datareader := io.NopCloser(io.MultiReader(strings.NewReader(s), strings.NewReader("\n")))
+	return datareader, nil
+}
+
+func (f echoOpener) Evaluate(s string) float32 {
+	// log.Debugf("Evaluating %s...\n", s)
+	if strings.HasPrefix(s, "echo://") {
+		return 0.9
+	}
+	return 0
+}
---
+++ pkg/openers/echo_test.go
@@ -0,0 +1,55 @@
+//go:build !fileonly
+// +build !fileonly
+
+package openers_test
+
+import (
+	"io"
+	"testing"
+
+	"github.com/batmac/ccat/pkg/openers"
+)
+
+func Test_echoOpener_Open(t *testing.T) {
+	tests := []struct {
+		name    string
+		args    string
+		wantErr bool
+		wanted  string
+	}{
+		{name: "ok", args: "echo://salut", wantErr: false, wanted: "salut\n"},
+		{name: "ko", args: "echo://", wantErr: true, wanted: ""},
+		{name: "ok double-quoted", args: "echo://\"salut yo\"", wantErr: false, wanted: "salut yo\n"},
+		{name: "ok single-quoted", args: "echo://'salut again'", wantErr: false, wanted: "salut again\n"},
+		{name: "ko double-quoted", args: "echo://\"\"", wantErr: true, wanted: ""},
+		{name: "ko single-quoted", args: "echo://''", wantErr: true, wanted: ""},
+		{name: "emoji", args: "echo://👍 🥳", wantErr: false, wanted: "👍 🥳\n"},
+		{name: "with 'zero' byte in it", args: "echo://salut\x00and again", wantErr: false, wanted: "salut\x00and again\n"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := openers.Open(tt.args, false)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("echoOpener.Open() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if got == nil && !tt.wantErr {
+				t.Errorf("echoOpener.Open() = got nil")
+				return
+			}
+			if tt.wantErr {
+				return
+			}
+			data, err := io.ReadAll(got)
+			if err != nil {
+				t.Errorf("echoOpener.Open() = %v", err)
+				return
+			}
+			if string(data) != tt.wanted {
+				t.Errorf("echoOpener.Open() = %#v, want %#v", string(data), tt.wanted)
+				return
+			}
+		})
+	}
+}
