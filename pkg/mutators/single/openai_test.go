f73b11e20d5d1d54d7d40f7fec4a97b4bf215c55
Baptiste Canton <bat@sbz.fr>
2023-04-07 19:21:00 +0200

    fix openai test

--- pkg/mutators/single/openai_test.go
+++ pkg/mutators/single/openai_test.go
@@ -10,9 +10,9 @@ func Test_chatgpt(t *testing.T) {
 	// only test that we do not panic
 	t.Setenv("OPENAI_API_KEY", "CI")
 
-	f := "chatgpt"
+	f := "chatgpt:100:fakemodel"
 	t.Run("donotpanicplease", func(t *testing.T) {
-		if got := mutators.Run(f, "hi"); got == "fake" {
+		if got := mutators.Run(f, "hi"); got != "CI" {
 			t.Errorf("%s = %v, want %v", f, got, "")
 		}
 	})
