7a392af6274648d51d4749e54cecc70516b1a740
Baptiste Canton <bat@sbz.fr>
2023-04-28 15:07:24 +0200

    add a plugin example for Deepl translation (#327)

---
+++ example/plugins/deepl.go
@@ -0,0 +1,86 @@
+package plugins
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+// ccat -m yaegi:example/plugins/deepl.go:plugins.Deepl echo://"bonjour"
+
+type deeplTranslation struct {
+	DetectedSourceLanguage string `json:"detected_source_language"` //nolint:tagliatelle
+	Text                   string `json:"text"`
+}
+
+type deeplTranslateResponse struct {
+	Translations []deeplTranslation `json:"translations"`
+}
+
+func Deepl(w io.WriteCloser, r io.ReadCloser, _ any) (int64, error) {
+	targetLanguage := os.Getenv("TARGET_LANGUAGE")
+	if targetLanguage == "" {
+		targetLanguage = "en"
+	}
+
+	text, err := io.ReadAll(r) // NOT streamable
+	if err != nil {
+		return 0, err
+	}
+	if len(text) == 0 {
+		return 0, nil
+	}
+
+	key := os.Getenv("DEEPL_API_KEY")
+	if key == "" {
+		log.Fatal("no key found in $DEEPL_API_KEY")
+	}
+
+	baseURL := "https://api-free.deepl.com/v2/translate"
+	// if apikey doesnt end with ":fx", use paid API
+	if key[len(key)-3:] != ":fx" {
+		baseURL = "https://api.deepl.com/v2/translate"
+	}
+
+	data := url.Values{}
+	data.Set("text", string(text))
+	data.Set("target_lang", targetLanguage)
+
+	// Create request
+	req, err := http.NewRequest(http.MethodPost, baseURL, bytes.NewBufferString(data.Encode()))
+	if err != nil {
+		return 0, err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Authorization", "DeepL-Auth-Key "+key)
+
+	// Send request
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Println("deepl response status:", resp.Status)
+		return 0, errors.New(resp.Status)
+	}
+
+	// Parse response
+	var translateResp deeplTranslateResponse
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&translateResp)
+	if err != nil {
+		return 0, err
+	}
+
+	log.Println("detected source language: ", translateResp.Translations[0].DetectedSourceLanguage)
+	return io.Copy(w, strings.NewReader(translateResp.Translations[0].Text))
+}
