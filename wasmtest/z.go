e0d79301bed3fc24d1fb56b3e4dc241fe05cabd7
Baptiste Canton <bat@sbz.fr>
2024-09-14 02:29:39 +0200

    pit

--- wasmtest/z.go
+++ wasmtest/z.go
@@ -8,8 +8,9 @@ import (
 	"os"
 
 	"github.com/tetratelabs/wazero"
-	// "github.com/tetratelabs/wazero/api"
 	"github.com/tetratelabs/wazero/imports/wasi_snapshot_preview1"
+	"github.com/tetratelabs/wazero/sys"
+	// "github.com/tetratelabs/wazero/api"
 )
 
 func main() {
@@ -20,21 +21,12 @@ func main() {
 	defer r.Close(ctx)
 
 	// Instantiate WASI in the runtime
-	_, err := wasi_snapshot_preview1.Instantiate(ctx, r)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to instantiate WASI: %v", err))
-	}
+	wasi_snapshot_preview1.MustInstantiate(ctx, r)
 
 	// Load your WASM module
 	wasmBytes, err := os.ReadFile("main.wasm")
 	if err != nil {
 		panic(fmt.Sprintf("Failed to read WASM module: %v", err))
-	}
-
-	// Compile the WASM module
-	compiled, err := r.CompileModule(ctx, wasmBytes)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to compile WASM module: %v", err))
 	}
 
 	// Create an in-memory file system for stdin/stdout using bytes.Buffer
@@ -51,17 +43,19 @@ func main() {
 		WithStderr(os.Stderr) // Set stderr to the standard error of the host process
 
 	// Instantiate the WASM module with the configured input/output
-	instance, err := r.InstantiateModule(ctx, compiled, config)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to instantiate WASM module: %v", err))
+	if _, err := r.InstantiateWithConfig(ctx, wasmBytes, config); err != nil {
+		if exitErr, ok := err.(*sys.ExitError); ok && exitErr.ExitCode() != 0 {
+			fmt.Fprintf(os.Stderr, "exit_code: %d\n", exitErr.ExitCode())
+		} else if !ok {
+			panic(err)
+		}
 	}
-	defer instance.Close(ctx)
 
 	// Run the "_start" function, which is the standard entry point for WASI modules
-	_, err = instance.ExportedFunction("_start").Call(ctx)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to run WASM module: %v", err))
-	}
+	// _, err = instance.ExportedFunction("_start").Call(ctx)
+	// if err != nil {
+	// panic(fmt.Sprintf("Failed to run WASM module: %v", err))
+	// }
 
 	// Read and print the output from the WASM module
 	fmt.Printf("Output from WASM: %s", stdout.String())
